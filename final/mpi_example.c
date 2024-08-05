2e3e107b0fab2a04f024ccd2f76e1e93227cb511
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2024-08-05 10:34:27 +0530

    Reformat code readability

--- final/mpi_example.c
+++ final/mpi_example.c
@@ -1,17 +1,22 @@
 #include <mpi.h>
 #include <stdio.h>
-int main(int argc, char** argv) {
-MPI_Init(NULL, NULL);
-int rank;
-MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-if (rank == 0) {
-int data = 100;
-MPI_Send(&data, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-} else if (rank == 1) {
-int data;
-MPI_Recv(&data, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-printf("Process 1 received data: %d\n", data);
-}
-MPI_Finalize();
-return 0;
+
+int main(int argc, char **argv)
+{
+    MPI_Init(NULL, NULL);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0)
+    {
+        int data = 100;
+        MPI_Send(&data, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    }
+    else if (rank == 1)
+    {
+        int data;
+        MPI_Recv(&data, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 1 received data: %d\n", data);
+    }
+    MPI_Finalize();
+    return 0;
 }
