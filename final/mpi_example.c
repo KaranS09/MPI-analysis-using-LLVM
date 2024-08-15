68fd67f6f8f77f75138d2c423aef97dc2d25d403
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2024-08-15 12:32:42 +0530

    Comments

--- final/mpi_example.c
+++ final/mpi_example.c
@@ -14,40 +14,36 @@ int main(int argc, char **argv)
     // Communication in MPI_COMM_WORLD (Default Communicator)
     // --------------------------------------------
 
-    if (rank == 0)
+    if (rank == 0) // Process 0
     {
-        int data = 100; // Data to send
-        // Process 0 sends data to Process 1 with tag 0
-        MPI_Send(&data, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        int data = 100;                                    // Data to send
+        MPI_Send(&data, 1, MPI_INT, 1, 0, MPI_COMM_WORLD); // Process 0 sends data to Process 1 with tag 0
     }
-    else if (rank == 1)
+    else if (rank == 1) // Process 1
     {
-        int data; // Buffer to receive data
-        // Process 1 receives data from Process 0 with tag 0
-        MPI_Recv(&data, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        int data;                                                             // Buffer to receive data
+        MPI_Recv(&data, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // Process 1 receives data from Process 0 with tag 0
         printf("Process 1 received data: %d\n", data);
     }
 
     // --------------------------------------------
     // Custom Communicator (custom_comm)
     // --------------------------------------------
-    // Split MPI_COMM_WORLD into two groups: even ranks and odd ranks
+    // Split MPI_COMM_WORLD into two groups: even ranks & odd ranks
     // Each group forms a new communicator
 
     MPI_Comm custom_comm;
     MPI_Comm_split(MPI_COMM_WORLD, rank % 2, rank, &custom_comm);
 
-    if (rank == 2)
+    if (rank == 2) // Process 2
     {
-        int data = 200; // Data to send
-        // Process 2 sends data to Process 3 with tag 1 within custom_comm
-        MPI_Send(&data, 1, MPI_INT, 3, 1, custom_comm);
+        int data = 200;                                 // Data to send
+        MPI_Send(&data, 1, MPI_INT, 3, 1, custom_comm); // Process 2 sends data to Process 3 with tag 1 within custom_comm
     }
-    else if (rank == 3)
+    else if (rank == 3) // Process 3
     {
-        int data; // Buffer to receive data
-        // Process 3 receives data from Process 2 with tag 1 within custom_comm
-        MPI_Recv(&data, 1, MPI_INT, 2, 1, custom_comm, MPI_STATUS_IGNORE);
+        int data;                                                          // Buffer to receive data
+        MPI_Recv(&data, 1, MPI_INT, 2, 1, custom_comm, MPI_STATUS_IGNORE); // Process 3 receives data from Process 2 with tag 1 within custom_comm
         printf("Process 3 received data: %d\n", data);
     }
 
@@ -55,17 +51,15 @@ int main(int argc, char **argv)
     // Adding More Processes with MPI_COMM_WORLD
     // --------------------------------------------
 
-    if (rank == 4)
+    if (rank == 4) // Process 4
     {
-        int data = 300; // Data to send
-        // Process 4 sends data to Process 5 with tag 2
-        MPI_Send(&data, 1, MPI_INT, 5, 2, MPI_COMM_WORLD);
+        int data = 300;                                    // Data to send
+        MPI_Send(&data, 1, MPI_INT, 5, 2, MPI_COMM_WORLD); // Process 4 sends data to Process 5 with tag 2
     }
-    else if (rank == 5)
+    else if (rank == 5) // Process 5
     {
-        int data; // Buffer to receive data
-        // Process 5 receives data from Process 4 with tag 2
-        MPI_Recv(&data, 1, MPI_INT, 4, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        int data;                                                             // Buffer to receive data
+        MPI_Recv(&data, 1, MPI_INT, 4, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // Process 5 receives data from Process 4 with tag 2
         printf("Process 5 received data: %d\n", data);
     }
 
@@ -78,17 +72,15 @@ int main(int argc, char **argv)
     MPI_Comm custom_comm2;
     MPI_Comm_split(MPI_COMM_WORLD, rank % 3, rank, &custom_comm2);
 
-    if (rank == 6)
+    if (rank == 6) // Process 6
     {
-        int data = 400; // Data to send
-        // Process 6 sends data to Process 7 with tag 3 within custom_comm2
-        MPI_Send(&data, 1, MPI_INT, 7, 3, custom_comm2);
+        int data = 400;                                  // Data to send
+        MPI_Send(&data, 1, MPI_INT, 7, 3, custom_comm2); // Process 6 sends data to Process 7 with tag 3 within custom_comm2
     }
-    else if (rank == 7)
+    else if (rank == 7) // Process 7
     {
-        int data; // Buffer to receive data
-        // Process 7 receives data from Process 6 with tag 3 within custom_comm2
-        MPI_Recv(&data, 1, MPI_INT, 6, 3, custom_comm2, MPI_STATUS_IGNORE);
+        int data;                                                           // Buffer to receive data
+        MPI_Recv(&data, 1, MPI_INT, 6, 3, custom_comm2, MPI_STATUS_IGNORE); // Process 7 receives data from Process 6 with tag 3 within custom_comm2
         printf("Process 7 received data: %d\n", data);
     }
 
